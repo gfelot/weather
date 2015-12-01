3e4cce2f9551a7e13289c51dc6aae8cb087acef1
Gil Felot <gfelot@gmail.com>
2015-12-01 13:12:46 +0100

    just clear the code from old one

--- Weather/ViewController.swift
+++ Weather/ViewController.swift
@@ -25,42 +25,13 @@ class ViewController: UIViewController {
     }
     
     @IBAction func getInfo(sender: AnyObject) {
-//        var flag = false
         let baseURL = "http://www.weather-forecast.com/locations/"
         let endURL = "/forecasts/latest"
         let city = cityField.text!.stringByReplacingOccurrencesOfString(" ", withString: "-")
         
         let testURL = NSURL(string: baseURL + city + endURL)
         
-//        if let url = testURL {
-//            let task = NSURLSession.sharedSession().dataTaskWithURL(url) { (data, response, error) -> Void in
-//                if let urlContent = data {
-//                    let webContent = NSString(data: urlContent, encoding: NSUTF8StringEncoding)
-//                    let websiteArray = webContent!.componentsSeparatedByString("3 Day Weather Forecast Summary:</b><span class=\"read-more-small\"><span class=\"read-more-content\"> <span class=\"phrase\">")
-//                    if websiteArray.count > 1 {
-//                        let weatherArray = websiteArray[1].componentsSeparatedByString("</span>")
-//                        if weatherArray.count > 1 {
-//                            let weatherSummary = weatherArray[0]
-//                            flag = true
-//                            // Don't forget to dispatch queue
-//                            dispatch_async(dispatch_get_main_queue(), { () -> Void in
-//                                self.labelField.text = weatherSummary.stringByReplacingOccurrencesOfString("&deg;", withString: "º")
-//                            })
-//                            
-//                        }
-//                    }
-//                }
-//            }
-//            if flag == false {
-//                self.labelField.text = "Couldn't find the weather for the city you are looking for - Please try again."
-//            } else {
-//                task.resume()
-//            }
-//        } else {
-//            self.labelField.text = "Couldn't find the weather for the city you are looking for - Please try again."
-//        }
-//        
-        
+        // With guard statement
         guard let url = testURL else {
             print("Test 1")
             self.labelField.text = "Couldn't find the weather for the city you are looking for - Please try again."
@@ -88,6 +59,35 @@ class ViewController: UIViewController {
                 
             }
         task.resume()
+        
+        //        if let url = testURL {
+        //            let task = NSURLSession.sharedSession().dataTaskWithURL(url) { (data, response, error) -> Void in
+        //                if let urlContent = data {
+        //                    let webContent = NSString(data: urlContent, encoding: NSUTF8StringEncoding)
+        //                    let websiteArray = webContent!.componentsSeparatedByString("3 Day Weather Forecast Summary:</b><span class=\"read-more-small\"><span class=\"read-more-content\"> <span class=\"phrase\">")
+        //                    if websiteArray.count > 1 {
+        //                        let weatherArray = websiteArray[1].componentsSeparatedByString("</span>")
+        //                        if weatherArray.count > 1 {
+        //                            let weatherSummary = weatherArray[0]
+        //                            flag = true
+        //                            // Don't forget to dispatch queue
+        //                            dispatch_async(dispatch_get_main_queue(), { () -> Void in
+        //                                self.labelField.text = weatherSummary.stringByReplacingOccurrencesOfString("&deg;", withString: "º")
+        //                            })
+        //
+        //                        }
+        //                    }
+        //                }
+        //            }
+        //            if flag == false {
+        //                self.labelField.text = "Couldn't find the weather for the city you are looking for - Please try again."
+        //            } else {
+        //                task.resume()
+        //            }
+        //        } else {
+        //            self.labelField.text = "Couldn't find the weather for the city you are looking for - Please try again."
+        //        }
+        //
     }
 }
 
